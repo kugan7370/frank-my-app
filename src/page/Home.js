472ee395968301d6473fd8db622c4185186dc3b1
kugan18 <kugapriyanj08@gmail.com>
2022-09-10 21:24:40 +0530

    Added delete   Confirm

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,76 +1,67 @@
-import React, { useState,useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-import { Toast } from 'react-toastify';
+import { Toast } from "react-toastify";
 import axios from "axios";
 
-import './Home.css';
-import personData from '../Data';
-
+import "./Home.css";
+import personData from "../Data";
 
 // Get user information from backend - API
 
-
-
 function Home() {
-  const [data, setdata] = useState(personData)
-  const [userEdit, setuserEdit] = useState(false)
+  const [data, setdata] = useState(personData);
+  const [userEdit, setuserEdit] = useState(false);
 
-  const handleclick=(id)=>{
-setdata(data.filter(i=>i.ID!==id))
-  }
-  
+  const handleclick = (id) => {
+    if (window.confirm("Are you sure to delete?")) {
+      setdata(data.filter((i) => i.ID !== id));
+    }
+  };
+
   return (
-   <div className='container'>
-    <div className='header'><h2>ELZIAN AGRO EMPLOYEES MANAGEMENT SYSTEM</h2></div>
-    <div className="App">
-    
-       <table>
-        <tr className='tableNav'>
-        <th>ID</th>
-          <th>Emp Name</th>
-          <th>Emp Email</th>
-          <th>Emp Contact</th>
-          <th>Emp Action</th>
-        </tr>
-        <tbody>
-        {data.map((val, key,) => {
-          return (
-            <tr key={key}>
-              
-              <td>
-                
-                {val.ID}
-              
-              </td>
-              <td>{val.name}</td>
-              <td>{val.mail}</td>
-              <td>{val.contact}</td>
-              <td >
-              
-              <div className='buttons'>
-              <Link to={`/edit/${val.ID}`}>
-                  <button className='btn btn-Edit'>Edit</button>
-                  </Link>
-                  <div onClick={()=>handleclick(val.ID)}>
-                  <button className='btn btn-Delete'>Delete</button>
-                  </div>
-                  <Link to={`/view/${val.ID}`}>
-                  <button className='btn btn-View'>View</button>
-                  </Link>
-                  </div>
-              
-              </td>
+    <div className="container">
+      <div className="header">
+        <h2>ELZIAN AGRO EMPLOYEES MANAGEMENT SYSTEM</h2>
+      </div>
+      <div className="App">
+        <table>
+          <tr className="tableNav">
+            <th>ID</th>
+            <th>Emp Name</th>
+            <th>Emp Email</th>
+            <th>Emp Contact</th>
+            <th>Emp Action</th>
+          </tr>
+          <tbody>
+            {data.map((val, key) => {
+              return (
+                <tr key={key}>
+                  <td>{val.ID}</td>
+                  <td>{val.name}</td>
+                  <td>{val.mail}</td>
+                  <td>{val.contact}</td>
+                  <td>
+                    <div className="buttons">
+                      <Link to={`/edit/${val.ID}`}>
+                        <button className="btn btn-Edit">Edit</button>
+                      </Link>
+                      <div onClick={() => handleclick(val.ID)}>
+                        <button className="btn btn-Delete">Delete</button>
+                      </div>
+                      <Link to={`/view/${val.ID}`}>
+                        <button className="btn btn-View">View</button>
+                      </Link>
+                    </div>
+                  </td>
 
-
-
-              <td>{val.action}</td>
-            </tr>
-           
-          )
-        })}</tbody>
-      </table>
-    </div>
+                  <td>{val.action}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
